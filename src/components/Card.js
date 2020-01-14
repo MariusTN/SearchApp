e332ab06ee5365e43423d64909931d08e311c3de
MariusTN <pulsemusic8@gmail.com>
2020-01-14 17:59:55 +0000

    final version + CSS work - should be fully responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email}) => {
     
     return(
-        <div className='bg-light-blue dib br3 pa5 ma2 bw2 shadow-4 roboCard'>
+        <div className='roboCard'>
             <img alt='robots' src={`https://robohash.org/${name}?200x200`} ></img>
             <div>
                 <h2 className="roboName">{name}</h2>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
-import CardList from './CardList'
-import SearchBox from './SearchBox'
-import Scroll from './Scroll'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
 
 class App extends Component {
     constructor() {
@@ -22,27 +21,22 @@ class App extends Component {
     }
 
     render(){
-
-    const filteredRobots = this.state.robots.filter(robots =>{
-        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())})
-
+    const filteredRobots = this.state.robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())})
     if (this.state.robots.length === 0)
     {
         return <h1>Loading</h1>
     } 
-    else
-    {
+    else{
         return (
             <div className='tc'>
             <h1 className="title">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
+
             <CardList robots = {filteredRobots}/>
-            </Scroll> 
+
             </div>
             );
-    }
-            }
-}
+        }}}
 
 export default App;
